c8fdc2d45e03586bf08b6bdb4f976770a0303d5f
jackson igor <120987347+jacksondev267@users.noreply.github.com>
2023-02-23 08:34:43 -0300

    Ultima atualização

--- main.js
+++ main.js
@@ -54,8 +54,14 @@
 
 // Outra forma mais simples de fazer :
 
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  }
+  else{
+    console.log('Elemento não encontrado ou seletr inválido');
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -71,5 +77,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   tecla.onclick = function () {
     tocaSom(idAudio);
+  };
+
+  tecla.onkeydow = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
   };
 }
